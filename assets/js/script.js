54cb1a7c8b5203db549c3752e6670366c1dac7f8
Alejandro <alejandroidb@gmail.com>
2024-01-31 21:22:35 -0300

    indicar precio total a pagar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-precio = 400000
+precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+
+// let total = document.querySelector("p .valor-total")
+
+
+// total.innerHTML = precio * 
